2f73954be50e792a9554dcff8e8dce80bf70fb2b
Lucas Giancola <lgiancol@github.com>
2022-10-19 18:43:13 +0000

    Print all headers

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  console.log(`${new Date()} ${req.method} ${req.path}`);
-  res.send("Hello World!");
+    console.log(req.headers);
+    res.send("Hello World!");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
